ae4a56547fee138abe025f49247d9deab6c3b71b
shenyun <baixing696@163.com>
2022-04-01 16:06:59 +0800

    first commit

---
+++ src/main/java/com/webjuly/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.webjuly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author : guyun
+ * @date : 2022/4/1 15:53
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "hello world!";
+    }
+}
